052da9f7b2130c2daa7c42b0544c6d3589d936dc
base <base@192.168.100.116>
2014-01-20 18:59:30 +0900

    jquery plugin start

--- cmd/war/js/cmd.js
+++ cmd/war/js/cmd.js
@@ -1,5 +1,23 @@
-function Cmd(console) {
-	this.console = console;
-}
-Cmd.prototype.init = function() {
-}
+(function( $ ) { 
+    $.cmdConsole = function(options, consoleDiv) {
+    	this.settings = $.extend({
+    		info: "This is a web command line.",
+    		
+    	}, options);
+    	this.consoleDiv = consoleDiv;
+    	this.init();
+    };
+    $.extend($.cmdConsole, {
+    	prototype: {
+    		onEnter: function(element, event) {
+    			
+    		},
+    		init: function() {
+    		}
+    	}
+    
+    });
+    $.fn.cmdConsole = function(options) {
+		return new $.cmdConsole(options, this[0]);
+    }; 
+}( jQuery ));
