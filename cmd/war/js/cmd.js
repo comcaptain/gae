0644e0218d0976679c569f91f6a2c021c563401c
sunguoqiang_company <comcaptain@163.com>
2014-02-24 15:16:39 +0900

    modify javascript structure

--- cmd/war/js/cmd.js
+++ cmd/war/js/cmd.js
@@ -34,145 +34,148 @@ function Command(content, hint) {
 	this.valueRequired = false;
 	this.options = [];
 }
-Command.prototype.addOption = function(option) {
-	this.options[option.content] = option;
-};
-Command.prototype.toDisplayData = function() {
-	var data = new CmdDisplayTable(4);
-	data.withBorder = true;
-	data.addTr(["name", "hint", "require value", "can combine"]);
-	data.addTr([this.content, this.hint, this.valueRequired]);
-	for (var i in this.options) {
-		var option = this.options[i];
-		data.addTr(["-" + option.content, option.hint, option.valueRequired, option.canCombine]);
-	}
-	return data;
-};
-Command.prototype._preProcessCommand = function(str) {
-	str = str.replace(/\s+/g, " ");
-	str = str.trim();
-	return str;
-}
-Command.prototype._isOptionString = function(str) {
-	return str.indexOf("-") == 0;
-}
-Command.prototype._testHelp = function(str) {
-	return str.indexOf("--help") >= 0;
-}
-Command.prototype._toOptionValueStringPairs = function(str) {
-	var optionValueStringPairList = [];
-	var optionValueStringPair = [];
-	var parts = str.split(" ");
-	for (var i = 0; i < parts.length; i++) {
-		var part = parts[i];
-		if (this._isOptionString(part)) {
-			if (this._testHelp(part)) {
-				throw "help";
-			}
-			part = part.substr(1);
-			if (optionValueStringPair["value"]) {
-				optionValueStringPairList.push(optionValueStringPair);
-				optionValueStringPair = [];
-			}
-			if (!optionValueStringPair["option"]) optionValueStringPair["option"] = [part];
-			else optionValueStringPair["option"].push(part);
+Command.prototype = {
+	addOption: function(option) {
+		this.options[option.content] = option;
+	},
+	toDisplayData: function() {
+		var data = new CmdDisplayTable(4);
+		data.withBorder = true;
+		data.addTr(["name", "hint", "require value", "can combine"]);
+		data.addTr([this.content, this.hint, this.valueRequired]);
+		for (var i in this.options) {
+			var option = this.options[i];
+			data.addTr(["-" + option.content, option.hint, option.valueRequired, option.canCombine]);
 		}
-		else {
-			if (!optionValueStringPair["value"]) optionValueStringPair["value"] = [part];
-			else optionValueStringPair["value"].push(part);
-		}
-	}
-	if (optionValueStringPair != []) optionValueStringPairList.push(optionValueStringPair);
-	return optionValueStringPairList;
-}
-Command.prototype._getOptionObjFromOptionStr = function(optionStr) {
-	//if there is only one single option
-	if (this.options[optionStr]) return [this.options[optionStr]];
-	var optionObjs = [];
-	var isCombine = true;
-	var combineRuleRuinedOptionObjs = [];
-	for(var i = 0; i < optionStr.length; i++) {
-		var optionObj = this.options[optionStr.charAt(i)];
-		if (!optionObj) {
-			isCombine = false;
-			break;
-		}
-		if (!optionObj.canCombine) combineRuleRuinedOptionObjs.push(optionObj);
-		optionObjs.push(optionObj);
-	}
-	if (isCombine) {
-		if (combineRuleRuinedOptionObjs.length > 0) {
-			throw "Option -" + combineRuleRuinedOptionObjs[0]["content"] + " cannot be combined."
-		}
-		return optionObjs;
-	}
-	throw "Option -"  + optionStr + " is not supported!";
-}
-
-Command.prototype._referenceOptionObj = function(optionValueStringPairList) {
-	var optionValuePairList = [];
-	for (var i in optionValueStringPairList) {
-		var optionValueStringPair = optionValueStringPairList[i];
-		var optionValueObjPair = optionValueStringPair;
-		var valueRequiredOptionName = "";
-		if (optionValueStringPair["option"]) {
-			var optionStrList = optionValueStringPair["option"];
-			var optionObjList = [];
-			for (var j in optionStrList) {
-				var optionStr = optionStrList[j];
-				var optionObjs = this._getOptionObjFromOptionStr(optionStr);
-				for (var k in optionObjs) {
-					var optionObj = optionObjs[k];
-					optionObjList[optionObj.content] = optionObj;
-					if (optionObj.valueRequired) valueRequiredOptionName = optionObj.content;
+		return data;
+	},
+	_preProcessCommand: function(str) {
+		str = str.replace(/\s+/g, " ");
+		str = str.trim();
+		return str;
+	},
+	_isOptionString: function(str) {
+		return str.indexOf("-") == 0;
+	},
+	_testHelp: function(str) {
+		return str.indexOf("--help") >= 0;
+	},
+	_toOptionValueStringPairs: function(str) {
+		var optionValueStringPairList = [];
+		var optionValueStringPair = [];
+		var parts = str.split(" ");
+		for (var i = 0; i < parts.length; i++) {
+			var part = parts[i];
+			if (this._isOptionString(part)) {
+				if (this._testHelp(part)) {
+					throw "help";
 				}
+				part = part.substr(1);
+				if (optionValueStringPair["value"]) {
+					optionValueStringPairList.push(optionValueStringPair);
+					optionValueStringPair = [];
+				}
+				if (!optionValueStringPair["option"]) optionValueStringPair["option"] = [part];
+				else optionValueStringPair["option"].push(part);
 			}
-			optionValueObjPair["option"] = optionObjList;
+			else {
+				if (!optionValueStringPair["value"]) optionValueStringPair["value"] = [part];
+				else optionValueStringPair["value"].push(part);
+			}
 		}
-		if (valueRequiredOptionName && !optionValueObjPair["value"]) {
-			throw "Option -" +  valueRequiredOptionName + " requires value.";
+		if (optionValueStringPair != []) optionValueStringPairList.push(optionValueStringPair);
+		return optionValueStringPairList;
+	},
+	_getOptionObjFromOptionStr: function(optionStr) {
+		//if there is only one single option
+		if (this.options[optionStr]) return [this.options[optionStr]];
+		var optionObjs = [];
+		var isCombine = true;
+		var combineRuleRuinedOptionObjs = [];
+		for(var i = 0; i < optionStr.length; i++) {
+			var optionObj = this.options[optionStr.charAt(i)];
+			if (!optionObj) {
+				isCombine = false;
+				break;
+			}
+			if (!optionObj.canCombine) combineRuleRuinedOptionObjs.push(optionObj);
+			optionObjs.push(optionObj);
 		}
-		optionValuePairList.push(optionValueObjPair); 
-		valueRequiredOptionName = "";
+		if (isCombine) {
+			if (combineRuleRuinedOptionObjs.length > 0) {
+				throw "Option -" + combineRuleRuinedOptionObjs[0]["content"] + " cannot be combined."
+			}
+			return optionObjs;
+		}
+		throw "Option -"  + optionStr + " is not supported!";
+	},
+	_referenceOptionObj: function(optionValueStringPairList) {
+		var optionValuePairList = [];
+		for (var i in optionValueStringPairList) {
+			var optionValueStringPair = optionValueStringPairList[i];
+			var optionValueObjPair = optionValueStringPair;
+			var valueRequiredOptionName = "";
+			if (optionValueStringPair["option"]) {
+				var optionStrList = optionValueStringPair["option"];
+				var optionObjList = [];
+				for (var j in optionStrList) {
+					var optionStr = optionStrList[j];
+					var optionObjs = this._getOptionObjFromOptionStr(optionStr);
+					for (var k in optionObjs) {
+						var optionObj = optionObjs[k];
+						optionObjList[optionObj.content] = optionObj;
+						if (optionObj.valueRequired) valueRequiredOptionName = optionObj.content;
+					}
+				}
+				optionValueObjPair["option"] = optionObjList;
+			}
+			if (valueRequiredOptionName && !optionValueObjPair["value"]) {
+				throw "Option -" +  valueRequiredOptionName + " requires value.";
+			}
+			optionValuePairList.push(optionValueObjPair); 
+			valueRequiredOptionName = "";
+		}
+		return optionValuePairList;
+	},
+	analyzeCommand: function(str) {
+		str = this._preProcessCommand(str);
+		if (!str) return;
+		var optionValueStringPairList = this._toOptionValueStringPairs(str);
+		var optionValuePairList = this._referenceOptionObj(optionValueStringPairList);
+		return optionValueStringPairList;
+	},
+	//data format:
+	//[
+	// {
+	//   "option": "optionObjs",
+	//   "value": ["val1", "val2", ...]
+	// },
+	// {
+	//   "option": "optionObjs",
+	//   "value": ["val1", "val2", ...]
+	// }
+	//]
+	execute: function(data) {
+		if (data) return real_dump(data);
+		return "default execute";
 	}
-	return optionValuePairList;
 }
-Command.prototype.analyzeCommand = function(str) {
-	str = this._preProcessCommand(str);
-	if (!str) return;
-	var optionValueStringPairList = this._toOptionValueStringPairs(str);
-	var optionValuePairList = this._referenceOptionObj(optionValueStringPairList);
-	return optionValueStringPairList;
-}
-//data format:
-//[
-// {
-//   "option": "optionObjs",
-//   "value": ["val1", "val2", ...]
-// },
-// {
-//   "option": "optionObjs",
-//   "value": ["val1", "val2", ...]
-// }
-//]
-Command.prototype.execute = function(data) {
-	if (data) return real_dump(data);
-	return "default execute";
-};
 function CommandOption(content, hint) {
 	this.content = content;
 	this.hint = hint;
 	this.valueRequired = false;
 	this.canCombine = true;
-}
+};
 function CmdDisplayTable(columnCount) {
 	this.columnCount = columnCount;
 	this.withBorder = false;
 	this.trs = [];
-}
-CmdDisplayTable.prototype.addTr = function(tds) {
-	if (!this.columnCount) this.columnCount = tds.length;
-	this.trs.push(tds);
+};
+CmdDisplayTable.prototype = {
+	addTr: function(tds) {
+		if (!this.columnCount) this.columnCount = tds.length;
+		this.trs.push(tds);
+	}
 };
 (function( $ ) { 
     $.cmdConsole = function(options, $consoleDiv) {
